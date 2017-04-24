8b437c29d3bb95a51293e911bf7420a934b897c4
Chovin <Chovin@users.noreply.github.com>
2017-04-24 15:07:50 +1000

    move things around

--- index.js
+++ index.js
@@ -9,36 +9,62 @@ const Menu = electron.Menu;
 
 var mainWindow;
 var sysTray;
-app.on('ready', function(){
+
+
+function createWindow () {
+  var {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
+
+  mainWindow = new BrowserWindow({
+    width: width,
+    height: height,
+    resizable: false,
+    transparent: true,
+    frame: false,
+    fullscreen: false,
+    fullscreenable: false,
+    alwaysOnTop: true,
+    show: false  // fix startup flicker
+  });
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()  // fix startup flicker
+  });
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'html', 'main.html'),
+    protocol: 'file',
+    slashes: true,
+  }));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null  // cleanup stuff for later?
+  });
+};
+
+function createSysTrayMenu() {
+  sysTray = new Tray('assets/icon.png');
+
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Quit', type: 'normal', role: 'quit'}
+  ]);
+  sysTray.setToolTip('Wrong neighborhood...');
+  sysTray.setContextMenu(contextMenu);
+};
+
+
+app.on('ready', () => {
     // please test on other OSes
-    var {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
+    createWindow()
+    createSysTrayMenu()
+});
 
-    mainWindow = new BrowserWindow({
-      width: width,
-      height: height,
-      resizable: false,
-      transparent: true,
-      frame: false,
-      fullscreen: false,
-      fullscreenable: false,
-      alwaysOnTop: true,
-      show: false  // fix startup flicker
-    });
-    mainWindow.once('ready-to-show', () => {
-      mainWindow.show()  // fix startup flicker
-    });
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {  // imitate Mac OS X app behavior
+    app.quit()
+  }
+});
 
-    mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, 'html', 'main.html'),
-      protocol: 'file',
-      slashes: true,
-    }));
-
-    sysTray = new Tray('assets/icon.png');
-
-    const contextMenu = Menu.buildFromTemplate([
-      {label: 'Quit', type: 'normal', role: 'quit'}
-    ]);
-    sysTray.setToolTip('Wrong neighborhood...');
-    sysTray.setContextMenu(contextMenu);
+app.on('activate', function () {
+  if (mainWindow === null) {  // imitate Mac OS X app behavior
+    createWindow()
+  }
 });
