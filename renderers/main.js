49112c187f7cf13b345d71e66acfed42d2af2c35
Chovin <Chovin@users.noreply.github.com>
2017-04-25 15:50:31 +1000

    [engine] pixi base

---
+++ renderers/main.js
@@ -0,0 +1,44 @@
+// https://github.com/kittykatattack/learningPixi#installingpixiwithnodeandgulp
+
+const electron = require('electron');
+var PIXI = require('pixi.js');
+
+// var pixiApp = new PIXI.Application();
+
+// get the window size
+var remote = electron.remote;
+var browserWindow = remote.getCurrentWindow();
+
+// var canClickThrough = false;
+// browserWindow.setIgnoreMouseEvents(canClickThrough);
+
+var screen = electron.screen;
+var workAreaSize = electron.screen.getPrimaryDisplay().workAreaSize;
+
+var screen = document.getElementById('screen');
+// set screen canvas w/h
+screen.width = workAreaSize.width;
+screen.height = workAreaSize.height;
+
+var renderer = PIXI.autoDetectRenderer(
+	screen.width, screen.height,
+	{antialias: false, transparent: true, resolution: 1}
+);
+screen.appendChild(renderer.view);
+
+var stage = new PIXI.Container();
+
+PIXI.loader
+	.add("../pets/cookieman.gif")
+	.load(setup);
+
+function setup() {
+	var cman = new PIXI.Sprite(
+		PIXI.loader.resources["../pets/cookieman.gif"].texture
+	);
+
+	stage.addChild(cman);
+
+	renderer.render(stage);
+}
+
